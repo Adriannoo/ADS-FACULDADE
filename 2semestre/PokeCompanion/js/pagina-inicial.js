080d11f670989b052485bb4d53d3d432bbaf3637
Adriano Ramos dos Santos Junior <adriannoordsj@gmail.com>
2025-10-26 20:22:25 -0300

    DOM e local storage pronto

--- 2semestre/PokeCompanion/js/pagina-inicial.js
+++ 2semestre/PokeCompanion/js/pagina-inicial.js
@@ -2,21 +2,20 @@
 // Seleciona o ID do elemento <img>
 const sprite_blastoise = document.getElementById("blastoise-gif");
 
-
-// Função Async/Await
-// Função assíncrona, pode fazer outras coisas enquanto espera os dados.
-async function getPokemon(nome) {
-  try { // Vai tentar "try" executar o código
-    // o "fetch" vai fazer a requisição HTTP para a API
-    // "await" espera a resposta chegar antes de continuar
-    const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`);
-    const dados = await resposta.json(); // Converte a resposta JSON para objeto
-    console.log(dados); // Caso tudo certo, imprime os dados no console
-  } catch (erro) {
-    console.error("Erro:", erro); // Caso erro com os dados, joga o erro!
-  }
-}
-getPokemon("charmander");
+// // Função Async/Await
+// // Função assíncrona, pode fazer outras coisas enquanto espera os dados.
+// async function getPokemon(nome) {
+//   try { // Vai tentar "try" executar o código
+//     // o "fetch" vai fazer a requisição HTTP para a API
+//     // "await" espera a resposta chegar antes de continuar
+//     const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`);
+//     const dados = await resposta.json(); // Converte a resposta JSON para objeto
+//     console.log(dados); // Caso tudo certo, imprime os dados no console
+//   } catch (erro) {
+//     console.error("Erro:", erro); // Caso erro com os dados, joga o erro!
+//   }
+// }
+// getPokemon("charmander");
 
 // Busca o Sprite na PokeAPI
 fetch("https://pokeapi.co/api/v2/pokemon/blastoise")
